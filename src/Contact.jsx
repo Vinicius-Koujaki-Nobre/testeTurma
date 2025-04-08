ccb9c196d8091c7aec936037f8464c7968632cd8
Vinicius Nobre <vina.santos06@gmail.com>
2025-04-08 16:54:00 -0300

    Add files via upload

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,8 +1,31 @@
 import style from './Contact.module.css'
+import { MapContainer, TileLayer, useMap, Popup, Marker } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import 'leaflet-defaulticon-compatibility';
+import { Menu } from './components/menu';
 
-function Contact(){
-    return(
-        <h2 className={style.tt}>contato</h2>
+function Contact() {
+
+    const position = [-25.424847,-49.2749795]
+
+    return (
+        <>
+        <Menu option01='Voltar a página principal'/>
+            <h2 className={style.tt}>Mapa</h2>
+            <br />
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "auto", minHeight: "900px"}}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </>
     )
 }
 
---
+++ src/assets/mock/cards.js
@@ -0,0 +1,7 @@
+import photo1 from '../images/arara.webp'
+
+export const cards = [
+    {
+        text: "primeiro", img: photo1
+    }
+]
